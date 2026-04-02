5ee40f0855fdaa6e7ae4586686c284e5cfa71c66
than1217 <jonathan.figueroa08@gmail.com>
2026-04-02 20:13:24 +0800

    commit

--- src/config.py
+++ src/config.py
@@ -17,8 +17,8 @@ MYSQL_PATH = os.environ.get('MYSQL_PATH', "mysql.exe")
 # Destination database configuration
 DEST_DB_HOST = os.environ.get('DEST_DB_HOST', '10.10.10.133')
 DEST_DB_DATABASE = os.environ.get('DEST_DB_DATABASE', 'consultant_ods')
-# DEST_DB_USER = os.environ.get('DEST_DB_USER', 'jfiguero')
-# DEST_DB_PASSWORD = os.environ.get('DEST_DB_PASSWORD', 'jfigueroJF)#')
+DEST_DB_USER = os.environ.get('DEST_DB_USER', 'jfiguero')
+DEST_DB_PASSWORD = os.environ.get('DEST_DB_PASSWORD', 'jfigueroJF)#')
 
-DEST_DB_USER = os.environ.get('DEST_DB_USER', 'psalonga')
-DEST_DB_PASSWORD = os.environ.get('DEST_DB_PASSWORD', 'psalongaPS($')
+# DEST_DB_USER = os.environ.get('DEST_DB_USER', 'psalonga')
+# DEST_DB_PASSWORD = os.environ.get('DEST_DB_PASSWORD', 'psalongaPS($')
